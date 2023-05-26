783b4faac4b22f856a4dd7502f4d07930aadb918
Tim Dennis <tdennis@library.ucla.edu>
2023-05-25 18:46:23 -0700

    adding resumes

---
+++ setup_with_ddcv.R
@@ -0,0 +1,10 @@
+library(datadrivencv)
+library(here)
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Tim Dennis",
+  data_location = "https://docs.google.com/spreadsheets/d/1XIlYYvPkhFV8wS-dBNhJORaf3ho8dLsKIzA-rK0Z9p4",
+  pdf_location = "https://github.com/jt14den/cv/raw/master/dennis_cv.pdf",
+  #html_location = "nickstrayer.me/cv/",
+  source_location = "https://github.com/jt14den/cvv"
+)
